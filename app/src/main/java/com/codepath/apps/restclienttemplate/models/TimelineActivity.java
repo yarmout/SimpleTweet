cc36eaa84fbc1dda17a04f276751d1460c97d8e9
yarmout <58712447+yarmout@users.noreply.github.com>
2021-10-09 21:23:40 -0400

    Implement compose button foundation

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/TimelineActivity.java
@@ -2,7 +2,11 @@ package com.codepath.apps.restclienttemplate.models;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -61,6 +65,25 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setLayoutManager((new LinearLayoutManager(this)));
         rvTweets.setAdapter(adapter);
         populateHomeTimeline();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the action bar if they are present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.compose) {
+            // Compose icon has been selected
+            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
+            // Navigate to the compose activity
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
